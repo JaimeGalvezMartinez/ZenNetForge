5db69a50800cc1202e021b476436b5d05d5c52f3
Jaime Galvez Martinez <121205552+TheHellishPandaa@users.noreply.github.com>
2025-02-14 00:55:30 +0100

    Update ZenNet.sh

--- ZenNet.sh
+++ ZenNet.sh
@@ -1,7 +1,7 @@
 
  
 #!/bin/bash
-# Version 1.2 - December 2024
+# Version 1.4 - Febrary 2024
 # Developer: Jaime Galvez (TheHellishPandaa)
 # Description: Bash script for configuring gateway server, DHCP, SAMBA, OpenSSH, etc.
 # This Script is released under GNU General public license.
