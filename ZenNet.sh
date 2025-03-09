957fbc2415e4052b7b54cdc84e9a3482331d5c4e
Jaime Galvez Martinez <121205552+TheHellishPandaa@users.noreply.github.com>
2025-03-09 11:31:49 +0100

    Update ZenNet.sh

--- ZenNet.sh
+++ ZenNet.sh
@@ -1405,6 +1405,48 @@ echo "-----------------------------------"
 
 }
 
+make_backup () {
+
+clear
+ 
+echo "        ██████╗  █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗     ███████╗ ██████╗██████╗ ██╗██████╗ ████████╗ "
+echo "        ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗    ██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝ "
+echo "        ██████╔╝███████║██║     █████╔╝ ██║   ██║██████╔╝    ███████╗██║     ██████╔╝██║██████╔╝   ██║   "
+echo "        ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝     ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   "
+echo "        ██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║         ███████║╚██████╗██║  ██║██║██║        ██║   "
+echo "        ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝         ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   "
+echo "                                                                                                " 
+#make backup
+make_backup() {
+    local source=$1
+    local destination=$2
+    if [ -d "$source" ]; then
+        timestamp=$(date +"%Y%m%d_%H%M%S")
+        backup_file="$destination/backup_$timestamp.tar.gz"
+        sudo tar -czvf "$backup_file" -C "$source" .
+        echo "Backup created at: $backup_file"
+    else
+        echo "Source directory does not exist."
+    fi
+}
+# Prompt for source directory
+read -p "Enter the path of the directory you want to backup: " source_path
+
+# Prompt for destination path
+read -p "Enter the path where you want to save the backup: " destination_path
+
+# Create destination directory if it doesn't exist
+if [ ! -d "$destination_path" ]; then
+    sudo mkdir -p "$destination_path"
+    echo "Created destination directory: $destination_path"
+else
+    echo "Destination directory already exists: $destination_path"
+fi
+
+# make the backup
+make_backup "$source_path" "$destination_path"
+
+}
 
 # Main Menu
 while true; do
@@ -1432,7 +1474,8 @@ while true; do
     echo "14) Install Prometheus "
     echo "15) Install Graphana "
     echo "16) Show system Informaton "
-    echo "17) Exit"
+    echo "17) Make Backup "
+    echo "18) Exit"
     read -p "Choose an option: " opcion
 
     case $opcion in
@@ -1452,7 +1495,8 @@ while true; do
 	14) configure_prometheus ;;
  	15) configure_graphana ;;
   	16) show_system_info ;;
-        17) echo "Exiting. Goodbye!"; break ;;
+   	17) make_backup ;;
+        18) echo "Exiting. Goodbye!"; break ;;
         *) echo "Invalid option." ;;
     esac
 done
