a4b1e1f7c9f2086b711e1ef0a2ff450bda79b8db
Jaime Galvez Martinez <121205552+TheHellishPandaa@users.noreply.github.com>
2025-04-14 13:19:38 +0200

    Update ZenNet.sh

--- ZenNet.sh
+++ ZenNet.sh
@@ -1766,7 +1766,56 @@ get_INTERNET_CONNECTION
 echo "-----------------------------------"
 
 }
+update_php () {
 
+
+#!/bin/bash
+
+echo "=== Actualización de PHP 7.4 a PHP 8.1 en Ubuntu ==="
+
+# Paso 1: Add repository de Ondřej Surý
+echo "[1/6] Agregando repositorio de PHP..."
+sudo apt update && sudo apt upgrade
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update
+
+# Paso 2: Install PHP 8.1 And extensions
+echo "[2/6] Install PHP 8.1 And extensions..."
+sudo apt install -y php8.1 php8.1-cli php8.1-fpm php8.1-mysql php8.1-curl php8.1-xml php8.1-mbstring php8.1-zip php8.1-bcmath php8.1-soap php8.1-intl
+
+# Paso 3: Detect Web Server
+echo "[3/6] Detect Web Server"
+if systemctl is-active --quiet apache2; then
+    echo "Apache detectado. Configurando PHP 8.1 para Apache..."
+    sudo a2dismod php7.4
+    sudo a2enmod php8.1
+    sudo update-alternatives --set php /usr/bin/php8.1
+    sudo systemctl restart apache2
+elif systemctl is-active --quiet nginx; then
+    echo "Nginx detectado. Configurando PHP 8.1-FPM para Nginx..."
+    if grep -q "php7.4-fpm.sock" /etc/nginx/sites-available/*; then
+        echo "Actualizando sockets en configuración de Nginx..."
+        sudo sed -i 's/php7.4-fpm.sock/php8.1-fpm.sock/g' /etc/nginx/sites-available/*
+    fi
+    sudo update-alternatives --set php /usr/bin/php8.1
+    sudo systemctl restart php8.1-fpm
+    sudo systemctl restart nginx
+else
+    echo "No se detectó Apache ni Nginx. Por favor, configura el servidor web manualmente."
+fi
+
+# Paso 4: Verify PHP Version
+echo "[4/6] Verificando versión actual de PHP..."
+php -v
+
+echo "[5/6] Clean old packages
+sudo apt autoremove -y
+
+echo "[6/6] PHP 8.1 installed "
+php -v
+
+}
 
 # Main Menu
 while true; do
@@ -1797,7 +1846,8 @@ while true; do
     echo "17) Configure ACL "
     echo "18) Cerbot Management "
     echo "19) Make Backup or restore backup from ssh server "
-    echo "20) Exit"
+    echo "20) Update PHP Version "
+    echo "21) Exit"
     read -p "Choose an option: " opcion
 
     # case for execute the fuctions
@@ -1821,7 +1871,8 @@ while true; do
    	17) configure_acl ;;
         18) manage_certbot ;;
 	19) backup_or_restore_backup_from_ssh_server ;;
-        20) echo "Exiting. Goodbye!"; break ;;
+ 	20) update_php ;;
+        21) echo "Exiting. Goodbye!"; break ;;
         *) echo "Invalid option." ;;
     esac
 done
