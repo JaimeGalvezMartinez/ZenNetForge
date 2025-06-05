912b2f5127914bb54087bbfe6bcbec9744f0b0fc
Jaime Galvez Martinez <121205552+TheHellishPandaa@users.noreply.github.com>
2025-06-05 20:11:56 +0200

    Update ZenNet.sh

--- ZenNet.sh
+++ ZenNet.sh
@@ -1089,6 +1089,9 @@ NEXTCLOUD_VERSION=${NEXTCLOUD_VERSION:-"28.0.0"}  # Default version if user inpu
 read -p "Enter the database name (default: nextcloud_db): " DB_NAME
 DB_NAME=${DB_NAME:-"nextcloud_db"}
 
+read -p "Input Database User (Default nextcloud_user): " DB_USER
+DB_NAME=${DB_NAME:-"nextcloud_user"}
+
 while true; do
 # Start an infinite loop to keep prompting until the password is confirmed 
 
